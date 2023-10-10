387db23550ac5c58f0774c34d35fb07dca069fd4
ohsin020 <115971603+ohsin020@users.noreply.github.com>
2023-10-10 12:39:46 +0500

    Update HiHelloWorldWithSwitchCase.java

--- Java/HiHelloWorldWithSwitchCase.java
+++ Java/HiHelloWorldWithSwitchCase.java
@@ -6,7 +6,7 @@ public class HiHelloWorldWithSwitchCase {
         int ans = sin.nextInt();
         switch (ans){
             case 1->System.out.println(" H e l l o  W o r l d! ");
-            case 2->System.out.println("H i  W o r l d!");
+            case 2->System.out.println("H i  software engineering");
         }   
     }
 }
